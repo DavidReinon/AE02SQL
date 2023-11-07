cf93c7c65976761fc8807f9337689c19c422457d
DavidReinon <davidreinongarcia@gmail.com>
2023-11-07 21:43:51 +0100

    First commit

---
+++ src/Controlador.java
@@ -0,0 +1,18 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Controlador {
+
+	private Model Model;
+	private Vista Vista;
+
+	Controlador(Vista Vista, Model Model) {
+		this.Vista = Vista;
+		this.Model = Model;
+		Control();
+	}
+
+	public void Control() {
+
+	}
+}
